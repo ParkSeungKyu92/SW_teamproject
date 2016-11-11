c3c828fae97891acc5e4b81ac323bd8a2c60103f
Seung kyu <effortsk@naver.com>
2016-11-10 22:44:30 -0800

    main correct the writing

--- apps/app.c
+++ apps/app.c
@@ -1,11 +1,38 @@
 #include<stdio.h>
 #include"getch.h"
 #include"menu.h"
+#include"menu_long.h"
+char name[6][30] = {"Gulliver's Travels", "Narcissus","Rapunzel"
+			,"The Elves and the Shoemaker", "The Selfish Giant", "The Wind and the Sun"};
+char ch[] = "->";
 
 int main()
 {
-	system("clear");
-	int n = menu();
-	printf("%d",n);
+	int num = 0;
+	int level = 0;
+	while(1)
+	{
+		system("clear");
+		num = menu();
+		if(num == 1) //long text
+		{
+			level = menu_long();
+			if(level == -1)
+			{
+				
+				continue;
+			}
+			else // start
+			{
+				printf("success");
+				break;
+			}
+		}		
+		else if(num == 2) // short text
+		{
+			printf("success");
+			break;
+		}
+	}
 	return 0;
 }
